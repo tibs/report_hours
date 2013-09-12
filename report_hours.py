1e2935c7ebf2bbfbabdbb1bb54466b212cdc473e
Tibs <tibs@tonyibbs.co.uk>
2013-09-12 23:02:39 +0100

    Now with time spans

--- report_hours.py
+++ report_hours.py
@@ -14,6 +14,7 @@ from __future__ import print_function
 
 import sys
 import datetime
+import re
 
 class GiveUp(Exception):
     pass
@@ -22,6 +23,8 @@ MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
           'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 DAYS = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
+
+timespan_re = re.compile(r'(\d\d):(\d\d)\.\.(\d\d):(\d\d)')
 
 class Report(object):
 
@@ -138,7 +141,8 @@ class Report(object):
             ...          'Thu 12 Sep 8.0  -- a comment',
             ...          ':expect Mon=7.0',
             ...          ':year 2003',
-            ...          ' Wed 3 Sep  19.0   --  we  trim  edge  whitespace']
+            ...          ' Wed 3 Sep  19.0   --  we  trim  edge  whitespace',
+            ...          'Thu 4 Sep for 09:00..12:00 12:30..17:30 -- time spans']
             >>> for line in lines:
             ...    print(r.parse_line(line))
             None
@@ -148,13 +152,15 @@ class Report(object):
             None
             None
             ('Wed', 3, 'Sep', 2003, 19.0, 'we  trim  edge  whitespace')
+            ('Thu', 4, 'Sep', 2003, 8.0, 'time spans')
 
         but:
 
             >>> r.parse_line('Something something something -- fred')
             Traceback (most recent call last):
             ...
-            GiveUp: Expecting <day> <date> <month> <hours>
+            GiveUp: Expecting "<day> <date> <month> <hours>"
+            or "<day> <date> <month> for <timespan> [<timespan> ...]"
             not: 'Something something something'
 
             >>> r.parse_line('Something 3 Sep 2.0 -- not a day name')
@@ -182,6 +188,37 @@ class Report(object):
             Traceback (most recent call last):
             ...
             GiveUp: 12 Sep 2013 should be Thu, not Fri
+
+            >>> r.parse_line('Thu 12 Sep for fred -- too few ..')
+            Traceback (most recent call last):
+            ...
+            GiveUp: Timespan 'fred' is not <hh>:<mm>..<hh>:<mm>
+
+            >>> r.parse_line('Thu 12 Sep for 9:30..12:30..13:00 -- too many ..')
+            Traceback (most recent call last):
+            ...
+            GiveUp: Timespan '9:30..12:30..13:00' is not <hh>:<mm>..<hh>:<mm>
+
+            >>> r.parse_line('Thu 12 Sep for 999..09:30 -- no :')
+            Traceback (most recent call last):
+            ...
+            GiveUp: Timespan '999..09:30' is not <hh>:<mm>..<hh>:<mm>
+
+            >>> r.parse_line('Thu 12 Sep for 9:30..10:30 -- 09:30, not 9:30')
+            Traceback (most recent call last):
+            ...
+            GiveUp: Timespan '9:30..10:30' is not <hh>:<mm>..<hh>:<mm>
+
+            >>> r.parse_line('Thu 12 Sep for 09:30..fred -- no :')
+            Traceback (most recent call last):
+            ...
+            GiveUp: Timespan '09:30..fred' is not <hh>:<mm>..<hh>:<mm>
+
+            >>> r.parse_line('Thu 12 Sep for 09:30..08:30 -- backwards')
+            Traceback (most recent call last):
+            ...
+            GiveUp: Timespan '09:30..08:30' is not positive
+
         """
         line = line.strip()
         if not line or line[0] == '#':
@@ -200,11 +237,15 @@ class Report(object):
         comment = ' '.join(parts[1:]).lstrip()
 
         spec = data.split()
-        if len(spec) != 4:
-            raise GiveUp('Expecting <day> <date> <month> <hours>\n'
+        if len(spec) < 4 or (len(spec) > 4 and spec[3] != 'for'):
+            raise GiveUp('Expecting "<day> <date> <month> <hours>"\n'
+                         'or "<day> <date> <month> for <timespan> [<timespan> ...]"\n'
                          'not: {!r}'.format(data))
 
-        day, daynum, month, hours = spec
+        day = spec[0]
+        daynum = spec[1]
+        month = spec[2]
+
         if day.lower() not in self.hours_per_day:
             raise GiveUp('Expected 3 letter day name, not {!r}'.format(day))
         day = day.capitalize()
@@ -218,16 +259,41 @@ class Report(object):
         except ValueError:
             raise GiveUp('Expected integer date (day of month), not {!r}'.format(daynum))
 
-        try:
-            hours = float(hours)
-        except ValueError:
-            raise GiveUp('Expected floating point hours, not {!r}'.format(hours))
-
         #raise GiveUp(day, daynum, month, self.year, '=>', MONTHS.index(month))
         date = datetime.date(self.year, MONTHS.index(month)+1, daynum)
         if DAYS[date.weekday()] != day:
             raise GiveUp('{} {} {} should be {}, not {}'.format(daynum, month, self.year,
                          DAYS[date.weekday()], day))
+
+        if spec[3] == 'for':
+            spans = spec[4:]
+            hours = 0
+            for span in spans:
+                match = re.match(timespan_re, span)
+                if match is None or match.end() != len(span) or len(match.groups()) != 4:
+                    raise GiveUp('Timespan {!r} is not <hh>:<mm>..<hh>:<mm>'.format(span))
+                groups = match.groups()
+                try:
+                    start_time = datetime.timedelta(hours=int(groups[0]), minutes=int(groups[1]))
+                except ValueError:
+                    raise GiveUp('{}:{} is not a valid time, <hh>:<mm>'.format(groups[0], groups[1]))
+                try:
+                    end_time = datetime.timedelta(hours=int(groups[2]), minutes=int(groups[3]))
+                except ValueError:
+                    raise GiveUp('{}:{} is not a valid time'.format(groups[2], groups[3]))
+
+                if start_time >= end_time:
+                    raise GiveUp('Timespan {!r} is not positive'.format(span))
+
+                delta = end_time - start_time
+                delta_hours = delta.seconds / (60*60)
+                hours += delta_hours
+        else:
+            hours = spec[3]
+            try:
+                hours = float(hours)
+            except ValueError:
+                raise GiveUp('Expected floating point hours, not {!r}'.format(hours))
 
         return day, daynum, month, self.year, hours, comment
 
